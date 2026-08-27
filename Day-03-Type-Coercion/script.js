a56cfd8ee8487b796af46be9b582e8f866059611
HAJRA UMER <haj_jvd85@hotmail.com>
2026-08-27 10:14:12 +0100

    Add Day 03 Type Coercion

---
+++ Day-03-Type-Coercion/script.js
@@ -0,0 +1,13 @@
+const firstResult = 5 + "10";
+const secondResult = 8 - 10;
+const thirdResult = true + 5;
+const fourthResult = false + 8;
+const fifthResult = "10" * 2;
+const sixthResult = null + 22; // '510' (
+
+console.log(`5 + "10" = ${firstResult}`);
+console.log(`8 - 10 = ${secondResult}`);
+console.log(`1 + 5 = ${thirdResult}`);
+console.log(`0 + 8 = ${fourthResult}`);
+console.log(`"10" * 2 = ${fifthResult}`);
+console.log(`null + 22 = ${sixthResult}`);
