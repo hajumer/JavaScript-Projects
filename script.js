211af351a3f9c59faabb2c23c984fb9bdf029068
HAJRA UMER <haj_jvd85@hotmail.com>
2026-08-27 09:48:32 +0100

    Add Day 02 Sentence Maker

---
+++ script.js
@@ -0,0 +1,18 @@
+let adjective = "determined";
+let noun = "cricketer";
+let verb = "leading";
+let place = "Pakistan";
+let adjective2 = "inspiring";
+let noun2 = "his country";
+
+let firstStory = `Imran Khan was a ${adjective} ${noun} who played a crucial role in ${verb} the team to victory in ${place}. His leadership and dedication were truly ${adjective2}, making him a hero for ${noun2}.`;
+console.log(`${firstStory}`);
+
+adjective = "strong";
+noun = "leader";
+verb = "working for";
+place = "Pakistan";
+adjective2 = "Passionate";
+noun2 = "the people of Pakistan";
+let secondStory = `Imran Khan is a ${adjective} ${noun} who is ${verb} the betterment of ${place}. His efforts and commitment are truly ${adjective2}, earning him respect and admiration from ${noun2}.`;
+console.log(`${secondStory}`);
