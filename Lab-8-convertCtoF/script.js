97d72b7275bfd7eecd017a7531edb9da90df4234
HAJRA UMER <haj_jvd85@hotmail.com>
2026-09-10 09:04:08 +0100

    Celsius to Fahrenheit converter lab

---
+++ Lab-8-convertCtoF/script.js
@@ -0,0 +1,10 @@
+// Create a function that converts Celsius to Fahrenheit
+function convertCtoF(celsius) {
+  // Apply the Celsius to Fahrenheit conversion formula
+  return celsius * (9 / 5) + 32;
+}
+console.log(convertCtoF(0));
+console.log(convertCtoF(20));
+console.log(convertCtoF(100));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(-30));
